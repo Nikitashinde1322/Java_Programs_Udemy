46b6dacf10770597d0b32116840065cc043083b4
NikitaShinde <shindenikita132@gmail.com>
2025-02-21 18:46:49 +0530

    literals program

---
+++ P_4.java
@@ -0,0 +1,24 @@
+/*
+ * literals
+ */
+
+import java.lang.*;
+import java.util.*;
+
+public class P_4 
+{
+    public static void main(String args [])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        byte b1 = 10;
+        byte b2 = 0b1010;
+        byte b3 = 012;
+        byte b4 = 0XA;
+
+        System.out.println(" b1 : " + b1);
+        System.out.println(" b2 : " + b2);
+        System.out.println(" b3 : " + b3);
+        System.out.println(" b4 : " + b4);
+    }    
+}
