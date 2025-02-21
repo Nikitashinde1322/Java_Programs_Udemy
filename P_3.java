02d39f8c48d8d3b4fb7717042f504c6592715958
NikitaShinde <shindenikita132@gmail.com>
2025-02-21 18:22:34 +0530

    variable size program

---
+++ P_3.java
@@ -0,0 +1,18 @@
+/*
+ * variable size program
+ */
+
+import java.lang.*;
+import java.util.*;
+
+public class P_3 
+{
+    public static void main(String args [])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" Byte Min : " + Byte.MIN_VALUE);
+        System.out.println(" Byte Max : " + Byte.MAX_VALUE);
+        System.out.println(" Byte Bytes : " + Byte.BYTES);
+    }    
+}
