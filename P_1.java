d84635e14c5044729136a5b3fc457c097b9b1c35
NikitaShinde <shindenikita132@gmail.com>
2025-02-21 17:39:47 +0530

    hello world

---
+++ P_1.java
@@ -0,0 +1,14 @@
+/*
+ * Hello World Program
+ */
+
+ import java.lang.*;
+
+ public class P_1
+ {
+    public static void main(String args [])
+    {
+        System.out.println("Hello World");
+    }
+ }
+ 
