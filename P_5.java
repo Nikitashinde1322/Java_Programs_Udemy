aed9caede911b20eb4fc2b87b3748fa134a325e1
NikitaShinde <shindenikita132@gmail.com>
2025-02-21 19:23:25 +0530

    Datatype details program

---
+++ P_5.java
@@ -0,0 +1,18 @@
+/*
+ * Datatype details program
+ */
+
+import java.lang.*;
+import java.util.*;
+
+public class P_5 
+{
+    public static void main(String args [])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int x = 7;
+        System.out.println( Integer.toBinaryString (x));
+
+    }    
+}
