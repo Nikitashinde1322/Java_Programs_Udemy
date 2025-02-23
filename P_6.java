edcc208940a9c14f91c7a746c08c1f36b603196b
NikitaShinde <shindenikita132@gmail.com>
2025-02-23 15:54:37 +0530

    datatype program

---
+++ P_6.java
@@ -0,0 +1,17 @@
+/*
+ * datatype details program
+ */
+
+import java.lang.*;
+import java.util.*;
+
+public class P_6 
+{
+    public static void main(String args [])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int x = 12;
+        System.out.println( Integer.toOctalString (x) );
+    }    
+}
